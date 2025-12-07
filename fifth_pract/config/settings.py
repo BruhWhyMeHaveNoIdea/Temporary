48891324bdf5dde0004718da70f9428a52d006c0
Alexander <154541168+BruhWhyMeHaveNoIdea@users.noreply.github.com>
2025-12-07 17:59:49 +0500

    Update settings.py

--- fifth_pract/config/settings.py
+++ fifth_pract/config/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3yzrs+4g-81o5@*q$xrfv=-3*sqygzg*iqg&an$8l9x2jcp45'
-
+SECRET_KEY = '...'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
